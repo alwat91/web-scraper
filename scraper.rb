051530f712a37491e5beacca59bda4d3708b74ef
alan <alwat91@uga.edu>
2017-05-21 16:16:33 -0400

    services complete

--- scraper.rb
+++ scraper.rb
@@ -30,7 +30,28 @@ def parse_content(content)
   company["state"] = content.at_css("[itemprop=addressRegion]").text
   company["zip"] = content.at_css("[itemprop=postalCode]").text
 
-  puts company["zip"]
+  # Services
+  company_services = content.css(".company-services").css(".features").to_s
+  company["has_in_state"] = company_services.include?("In state")
+  company["has_out_state"] = company_services.include?("Out of state")
+  company["has_full_service"] = company_services.include?("Full-Service Move")
+  company["has_moving_labor"] = company_services.include?("Moving Labor")
+  company["has_packing_services"] = company_services.include?("Packing Services")
+  company["has_containers"] = company_services.include?("Portable Storage Containers")
+  company["has_agent"] = company_services.include?("Agent")
+  company["has_art_antiques"] = company_services.include?("Art")
+  company["has_auto_transport"] = company_services.include?("Auto Transport")
+  company["has_broker"] = company_services.include?("Broker")
+  company["has_carrier_broker"] = company_services.include?("Carrier/Broker")
+  company["has_commercial_moves"] = company_services.include?("Commercial/Business Moves")
+  company["has_corporate_reloc"] = company_services.include?("Corporate Relocation")
+  company["has_govt"] = company_services.include?("Government")
+  company["has_industrial_movers"] = company_services.include?("Industrial Movers")
+  company["has_dod_cert"] = company_services.include?("Military/DOD Certified")
+  company["has_pianos"] = company_services.include?("Pianos")
+  company["has_safes"] = company_services.include?("Safes")
+
+  puts company
 end
 
 def open_file(file)
@@ -40,11 +61,11 @@ end
 # content = get_content('http://www.movingcompanyreviews.com/AL/Birmingham/a-wise-move-inc-57486')
 content = open_file("sample1.html")
 parse_content(content)
-content = open_file("sample2.html")
-parse_content(content)
-content = open_file("sample3.html")
-parse_content(content)
-content = open_file("sample4.html")
-parse_content(content)
-content = open_file("sample5.html")
-parse_content(content)
+# content = open_file("sample2.html")
+# parse_content(content)
+# content = open_file("sample3.html")
+# parse_content(content)
+# content = open_file("sample4.html")
+# parse_content(content)
+# content = open_file("sample5.html")
+# parse_content(content)
