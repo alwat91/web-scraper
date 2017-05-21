cf81dde441ce2b3827eb505ed57bc63d25acadd1
alan <alwat91@uga.edu>
2017-05-21 13:44:08 -0400

    Added uri header

--- scraper.rb
+++ scraper.rb
@@ -5,7 +5,7 @@ def parse_uris
   sitemap = Nokogiri::XML(File.open("sitemap.xml"))
   uris = sitemap.css("loc")
 
-  CSV.open('data.csv', 'w') do |file|
+  CSV.open('data.csv', 'wb', :write_headers=> true, :headers => ["mcr_uri"]) do |file|
     uris.each do |uri|
       file << [uri.to_s[5..-7]]
     end
