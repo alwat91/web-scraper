367865434ed31f2ffc70adf25adac4aef5775ceb
alan <alwat91@uga.edu>
2017-05-21 13:36:42 -0400

    First commit

---
+++ scraper.rb
@@ -0,0 +1,16 @@
+require 'nokogiri'
+require 'csv'
+
+def parse_uris
+  sitemap = Nokogiri::XML(File.open("sitemap.xml"))
+  uris = sitemap.css("loc")
+
+  CSV.open('data.csv', 'wb', :write_headers => true, :headers => ["mcr_uri"]) do |file|
+    uris.each do |uri|
+      puts CSV.generate_line([uri.to_s[5..-7]])
+      # file << CSV.generate_line([uri.to_s[5..-7]])
+    end
+  end
+end
+
+parse_uris
